b3876534c8c0568e98569b3d539efb11847db43e
mbollo2005 <mbollo2005@gmail.com>
2019-11-07 10:48:50 +0100

    Adding code to Great HelloWorld man !!

--- demo.java
+++ demo.java
@@ -1,1 +1,15 @@
- public static  main(){ return 0 }
+
+
+// Add your imported classes here 
+
+public class HelloWorld
+{
+
+ 
+ // Add your new constructors  
+
+ public static  main()
+  {
+	 System.out.println("Hello World man !!")
+  }
+}
